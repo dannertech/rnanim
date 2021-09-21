9123ef1e84ffde4fc958e6d51f2aeabee7fb1a93
Diamonique Danner <diamoniquedanner@Diamoniques-MacBook-Pro.local>
2021-09-21 12:02:12 -0400

    fading animation on logo and sliding animation on textfields

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,9 +1,11 @@
-import {StyleSheet, Animated, Text, View, TextInput, Image} from 'react-native';
-import React, {useEffect, useRef} from 'react';
+import {StyleSheet, Animated, Text, View, TextInput, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 
 const LoginScreen = () => {
     
     const fade = useRef(new Animated.Value(0)).current;
+    const translate = useRef(new Animated.Value(0)).current;
+    const xPosition = new Animated.Value(0);
 
     useEffect(() => {
         Animated.timing(fade, 
@@ -11,16 +13,30 @@ const LoginScreen = () => {
         duration: 10000
         } 
             ).start();
-    }, [fade]);
+    
+    
+    Animated.timing(xPosition, {
+        toValue: -200,
+        duration: 1000
+    }).start();
+}
+    , [xPosition]);
     return(
         <Animated.View
         style={{
-            opacity: fade
+            backgroundColor: '#0A0D1D',
+            height: '100%'
         }}
         >
-            <Image source={require('../Images/tesla-shadow-logo.png')} style={{width: 250, height: 250, alignSelf: 'center', marginTop: 70}}/>
-            <TextInput placeholder="Email" style={{borderWidth: 2, borderRadius: 10, height: 40, marginHorizontal: 10}}/>
-            <TextInput placeholder="Password" style={{borderWidth: 2, borderRadius: 10, marginTop: 30, height: 40, marginHorizontal: 10}}/>
+            
+            <Animated.Image source={require('../Images/tesla-shadow-logo.png')} style={{opacity: fade,width: 250, height: 250, alignSelf: 'center', marginTop: 70}}/>
+            <Animated.View style={{transform: [{translateX: xPosition}]}}>
+            <TextInput placeholder="Email" style={{backgroundColor: 'white', borderWidth: 2, borderRadius: 10, height: 40, width: '90%', marginLeft: 210}}/>
+            <TextInput placeholder="Password" style={{backgroundColor: 'white', borderWidth: 2, borderRadius: 10, marginTop: 30, width: '90%', height: 40, marginLeft: 210}}/>
+            </Animated.View>
+            <TouchableOpacity>
+                <Text>Login</Text>
+            </TouchableOpacity>
         </Animated.View>
     )
 };
